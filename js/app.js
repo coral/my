418f9c5fc43d38e4deaf78c50201c64045e9041e
coral <jonas@jonasbengtson.se>
2013-08-04 12:51:46 +0200

    wat

--- js/app.js
+++ js/app.js
@@ -1,12 +1,4 @@
 require(['$api/models', '$views/image#Image'], function(models, Image) {
  
 });
-
-var controller = new Leap.Controller({enableGestures: true});
-      controller.loop(function(frame) {
-        latestFrame = frame;
-        document.getElementById('out').innerHTML =  "<div>"+ latestFrame.dump()+"</div>";
-      });
-      controller.on('ready', function() {
-          console.log("ready");
-      });
+    
