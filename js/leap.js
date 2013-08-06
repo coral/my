d59633f6578cfa3fefb4ed5f6dc22998dcf2b94c
Luuk van Egeraat <l.vanegeraat@gmail.com>
2013-08-06 15:15:45 +0200

    shoved in some moodpicker

--- js/leap.js
+++ js/leap.js
@@ -1,43 +1,81 @@
-var fingers = {};
-var wHeight = $(window).height();
-Leap.loop(function(frame) {
+$(function(){
+  var fingerIds,
+      fingers = {},
+      wHeight = $(window).height();
 
-	var fingerIds = {};
+  var setMoodPosition = (function(){
+    var $body = $(document.body),
+        $moodChooser = $('#mood-chooser'),
+        $valance = $('#valence'),
+        $energy = $('#energy'),
+        height = $moodChooser.height(),
+        width = $moodChooser.width(),
+        left = $moodChooser.position().left,
+        right = width + left,
+        top = $moodChooser.position().top,
+        bottom = height + top;
 
-        for (var pointableId = 0, pointableCount = frame.pointables.length; pointableId != pointableCount; pointableId++) {
-          var pointable = frame.pointables[pointableId];
-          var posX = (pointable.stabilizedTipPosition[0]*3)+400;
-          var posY = (wHeight-(pointable.stabilizedTipPosition[1]*3))+200;
-          var posZ = (pointable.stabilizedTipPosition[2]*3)-400;
-          var dirX = -(pointable.direction[0]*90);
-          var dirY = -(pointable.direction[1]*90);
-          var dirZ = (pointable.direction[2]*90);
-          var finger = fingers[pointable.id];
-          if (!finger) {
-            var fingerDiv = document.getElementById("finger").cloneNode(true);
-                fingerDiv.setAttribute('id',pointable.id);
-                fingerDiv.style.backgroundColor='#'+Math.floor(Math.random()*16777215).toString(16);
-                document.getElementById('container').appendChild(fingerDiv);
-                fingers[pointable.id] = pointable.id;
-          } else {
-            var fingerDiv =  document.getElementById(pointable.id);
-            if (typeof(fingerDiv) != 'undefined' && fingerDiv != null) {
-            	$(fingerDiv).css({left:posX,top:posY});
-              //moveFinger(fingerDiv, posX, posY, posZ, dirX, dirY, dirZ);
+    return function(posX, posY){
 
-            }
-          }
-          fingerIds[pointable.id] = true;
+      var valancy = (posX - left) / width,
+          energy = 1 - ((posY - top) / height);
+
+      if(posX > left && posX < right && posY > top && posY < bottom) {
+
+        $valance.text('valance: ' + valancy);
+        $energy.text('energy: ' + energy);
+
+        $body.css('background-color', 'hsl(' + (360 - Math.round(valancy * - 360)) + ',' + Math.round(energy * 80) + '%, 50%)');
+
+      }
+    }
+
+  }());
+
+  Leap.loop(function(frame) {
+
+    fingerIds = {};
+
+    for (var pointableId = 0, pointableCount = frame.pointables.length; pointableId != pointableCount; pointableId++) {
+      var pointable = frame.pointables[pointableId];
+      var posX = (pointable.stabilizedTipPosition[0]*3)+400;
+      var posY = (wHeight-(pointable.stabilizedTipPosition[1]*3))+200;
+      var posZ = (pointable.stabilizedTipPosition[2]*3)-400;
+      var dirX = -(pointable.direction[0]*90);
+      var dirY = -(pointable.direction[1]*90);
+      var dirZ = (pointable.direction[2]*90);
+      var finger = fingers[pointable.id];
+
+      setMoodPosition(posX, posY);
+
+      if (!finger) {
+        var fingerDiv = document.getElementById("finger").cloneNode(true);
+            fingerDiv.setAttribute('id',pointable.id);
+            fingerDiv.style.backgroundColor='#'+Math.floor(Math.random()*16777215).toString(16);
+            document.getElementById('container').appendChild(fingerDiv);
+            fingers[pointable.id] = pointable.id;
+      } else {
+        var fingerDiv =  document.getElementById(pointable.id);
+        if (typeof(fingerDiv) != 'undefined' && fingerDiv != null) {
+          $(fingerDiv).css({left:posX,top:posY});
+          //moveFinger(fingerDiv, posX, posY, posZ, dirX, dirY, dirZ);
+
         }
-        for (fingerId in fingers) {
-          if (!fingerIds[fingerId]) {
-          	
-            var fingerDiv =  document.getElementById(fingers[fingerId]);
-            fingerDiv.parentNode.removeChild(fingerDiv);
-           
-            delete fingers[fingerId];
-          }
-        }
+      }
+      fingerIds[pointable.id] = true;
+    }
 
+    for (fingerId in fingers) {
+      if (!fingerIds[fingerId]) {
+
+        var fingerDiv =  document.getElementById(fingers[fingerId]);
+        fingerDiv.parentNode.removeChild(fingerDiv);
+
+        delete fingers[fingerId];
+      }
+    }
+
+
+  });
 
 });
