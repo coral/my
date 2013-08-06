910664cec96c088850fc1fec877f65554cf44be7
Jonas Bengtson <jonas@jonasbengtson.se>
2013-08-06 21:36:15 +0200

    stuf

--- js/app.js
+++ js/app.js
@@ -1,16 +1,25 @@
+var minenergy, maxenergy, minvalence, maxvalence;
+
 var app = {
 
 	initSpotify: function() {
 
 	},
 
-	getUserToplist: function() {
-		require(['$api/toplists'], function(toplists) {
+	buildSuggestions: function(minenergyz, maxenergyz, minvalencez, maxvalencez) {
+		minenergy = minenergyz;
+		maxenergy = maxenergyz;
+		minvalence = minvalencez;
+		maxvalence = maxvalencez;
 
+		require(['$api/toplists'], function(toplists) {
+			var an = new Array();
 			var list = toplists.Toplist.forCurrentUser();
 			list.artists.snapshot().done(function(artist) {
-			  for (var i = 0; i < artist.length; i++)
-			    app.buildTasteProfile(artist.get(i));
+			  	for (var i = 0; i < 4; i++){
+					an.push(artist.get(i).name);
+				}
+				app.queryEchonest(an);
 			});
 
 		});
@@ -42,7 +51,11 @@ var app = {
 						var st = songs[i].tracks[0].foreign_id;
 						st = st.replace("spotify-WW","spotify");
 						tasteProfile.tracks.add(models.Track.fromURI(st));
+						console.log(models.Track.fromURI(st));
 					}
+					tasteProfile.tracks.snapshot().done(function(playlist) {
+						console.log(playlist);
+					});
 
 				}
 			}
@@ -51,29 +64,27 @@ var app = {
 
 	},
 
-	buildSuggestions: function(minenergy, maxenergy, minvalence, maxvalence) {
-		$.getJSON('http://developer.echonest.com/api/v4/song/' +
-			'search?api_key=7XGOU94ICDTSF1A2I' +
-			'&format=json' + 
-			'&results=10' +
-			'&max_energy=' + maxenergy +
-			'&min_energy=' + minenergy +
-			'&min_valence=' + minvalence +
-			'&max_valence=' + maxvalence +
-			'&bucket=id:spotify-WW&bucket=tracks&limit=true'
-			, function(data) {
-				app.parseTasteProfile(data);
-		});
-	},
+	queryEchonest: function(artists) {
+		var add;
+		for(var i = 0; i < artists.length; i++)
+		{
+			if (add != null)
+			{
+				add = add + "&artist=" + artists[i];
+			} else {
+				add = "&artist=" + artists[i];
+			}
+			
+		}
 
-	buildTasteProfile: function(wat)
-	{
-		console.log(wat);
-/*
-		$.getJSON('http://developer.echonest.com/api/v4/song/' +
-			'search?api_key=7XGOU94ICDTSF1A2I' +
+		//http://developer.echonest.com/api/v4/playlist/static?api_key=7XGOU94ICDTSF1A2I&artist=weezer&artist=Kraftwerk&artist=Peter%20Tosh&format=json&results=2&type=artist-radio&bucket=id:spotify-WW&bucket=tracks&limit=true&max_energy=.5&min_energy=.1&max_valence=.3&min_valence=.1
+
+		$.getJSON('http://developer.echonest.com/api/v4/playlist/' +
+			'static?api_key=7XGOU94ICDTSF1A2I' +
+			add +
 			'&format=json' + 
 			'&results=10' +
+			'&type=artist-radio' +
 			'&max_energy=' + maxenergy +
 			'&min_energy=' + minenergy +
 			'&min_valence=' + minvalence +
@@ -82,12 +93,11 @@ var app = {
 			, function(data) {
 				app.parseTasteProfile(data);
 		});
-*/
 	}
 };
 
 $(function() {
 	app.initSpotify();
-	app.getUserToplist();
 	app.buildSuggestions(0.5, 0.8, 0.5, 0.8);
+	//app.buildSuggestions(0.5, 0.8, 0.5, 0.8);
 });
