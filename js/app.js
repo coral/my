a7618b030d695b28df21e5d5a3dead9a36d0ce34
Jonas Bengtson <jonas@jonasbengtson.se>
2013-08-06 16:10:42 +0200

    worked on app stuff

--- js/app.js
+++ js/app.js
@@ -1,18 +1,30 @@
 var sp, models, views;
 
 var app = {
+
 	initSpotify: function() {
 		sp = getSpotifyApi();
 		models = sp.require('$api/models');
 		views = sp.require('$api/views');
 	},
 
-	animateCanvas: function() {
-
+	buildSuggestions: function() {
+		$.getJSON('ajax/test.json', function(data) {
+		  var items = [];
+		 
+		  $.each(data, function(key, val) {
+		    items.push('<li id="' + key + '">' + val + '</li>');
+		  });
+		 
+		  $('<ul/>', {
+		    'class': 'my-new-list',
+		    html: items.join('')
+		  }).appendTo('body');
+		});
 	}
-
 };
 
 $(function() {
 	app.initSpotify();
+	app.buildSuggestions();
 });
